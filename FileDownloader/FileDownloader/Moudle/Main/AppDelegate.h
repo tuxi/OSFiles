bc1fbf239a30181ed290042845b58700a80cdc1e
Ossey <xiaoyuan1314@me.com>
2017-06-10 14:38:59 +0800

    initialize project

---
+++ FileDownloader/FileDownloader/Moudle/Main/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FileDownloader
+//
+//  Created by Ossey on 2017/6/10.
+//  Copyright © 2017年 Ossey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FileDownloader/FileDownloader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FileDownloader
+//
+//  Created by Ossey on 2017/6/10.
+//  Copyright © 2017年 Ossey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
