4043d5223779e1034d75682228fed37e730b61f9
Ossey <xiaoyuan1314@me.com>
2017-10-20 00:01:40 +0800

    Update example.

--- FileDownloader/FileDownloader/Moudle/Browser/View/BrowserViewController.m
+++ FileDownloader/FileDownloader/Moudle/Browser/View/BrowserViewController.m
@@ -32,6 +32,10 @@
     
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self.textField resignFirstResponder];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
