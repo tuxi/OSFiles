09e8dd79560f4345c616f130ba414ba0e1ecafde
Ossey <xiaoyuan1314@me.com>
2017-10-31 21:10:56 +0800

    Add ApplicationHelper

---
+++ FileDownloader/Helper/ApplicationHelper.h
@@ -0,0 +1,13 @@
+//
+//  ApplicationHelper.h
+//  FileDownloader
+//
+//  Created by Swae on 2017/10/31.
+//  Copyright © 2017年 Ossey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ApplicationHelper : NSObject
+
+@end
---
+++ FileDownloader/Helper/ApplicationHelper.m
@@ -0,0 +1,14 @@
+
+//
+//  ApplicationHelper.m
+//  FileDownloader
+//
+//  Created by Swae on 2017/10/31.
+//  Copyright © 2017年 Ossey. All rights reserved.
+//
+
+#import "ApplicationHelper.h"
+
+@implementation ApplicationHelper
+
+@end
